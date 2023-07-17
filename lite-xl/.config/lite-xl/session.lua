4a4a5420fa64bbdfb0b6f579c7bffb7a15cbdc02
Tuhkis <nicolindstromj@gmail.com>
2023-07-17 22:07:49 +0300

    lite-xl

---
+++ lite-xl/.config/lite-xl/session.lua
@@ -0,0 +1,1 @@
+return {recents={}, window={[1]=1334,[2]=1038,[3]=10,[4]=32,["n"]=4}, window_mode="normal", previous_find={}, previous_replace={}}
---
+++ lite-xl/.config/lite-xl/user_settings.lua
@@ -0,0 +1,15 @@
+return {
+  ["config"] = {
+    ["animate_drag_scroll"] = false,
+    ["blink_period"] = 0.3,
+    ["borderless"] = false,
+    ["indent_size"] = 4,
+    ["max_tabs"] = 7,
+    ["statusbar"] = {
+      ["enabled"] = false,
+      ["messages"] = false
+    },
+    ["tab_type"] = "hard",
+    ["transitions"] = false
+  }
+}
